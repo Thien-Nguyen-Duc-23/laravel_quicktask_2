4e915c5ea3132a9c479bb3fed8c6ab014f1adf0d
Thien-Nguyen-Duc-23 <tducnguyen1997@gmail.com>
2018-06-12 15:23:06 +0700

    create Migration_Model_Seeding

---
+++ quickstart/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        DB::table('users')->insert([
+            'name' => $faker->name,
+        ]);
+    }
+}
